0380509a58533362a90a96fc8ee6aa1f502513c0
Marc Poppy <poppy.marc@gmail.com>
2019-11-17 22:15:41 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -53,7 +53,7 @@ class InteractiveRecord
   end
 
   def self.find_by(options={})
-    sql = "SELECT * FROM #{self.table_name} WHERE '#{propery}' = #{value}"
+    sql = "SELECT * FROM #{self.table_name} WHERE '#{property}' = #{value}"
     DB[:conn].execute(sql, hash)
   end
 
