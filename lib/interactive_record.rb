bd5b470a5570fb12480e02a9c148b64c707fdcd3
Marc Poppy <poppy.marc@gmail.com>
2019-11-17 17:57:15 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,31 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "pragma table_info('#{table_name}')" #pragma used to query non-table data
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+    column_names.compact
+  end
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
   
+
+
+
 end
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,8 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+  #class needed to use the attr_accessors specific to this class and make as columns
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 end
